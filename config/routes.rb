ec245ba293f6ac3f0a7415a8ee66d7dc4cc3c1aa
Albert Vives <albert.avipe@gmail.com>
2017-09-16 23:04:55 +0200

    Install Devise + Windows fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'static_pages/home'
+
+  devise_scope :user do
+    get     '/signin',    to: 'devise/sessions#new'
+#                          as: :new_user_session
+    post    '/signin',    to: 'devise/sessions#create'
+#                          as: :user_session
+    delete  '/signout',   to: 'devise/sessions#destroy'
+#                          as: :destroy_user_session
+
+    get     '/signup',    to: 'devise/registrations#new'
+#                          as: :new_registration
+    post    '/signup',    to: 'devise/registrations#create'
+#                          as: :registration
+  end
+  devise_for :users
 end
